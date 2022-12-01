bb577b74ca8724915274a1cb2be3d6c58b87c1b8
nachat-ayoub <venomx2020x@gmail.com>
2022-12-01 14:49:42 +0100

    Fix Types Issues in Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,15 +5,13 @@ import { View, Text, StatusBar, TouchableOpacity } from "react-native";
 import { Header as HeaderRN } from "@rneui/base";
 import React, { FunctionComponent, ReactElement } from "react";
 
-
 type Props = {
-  backgroundColor?: string,
-  height?: string,
-  leftComponent?: JSX.Element | ReactElement | FunctionComponent,
-  midComponent?: JSX.Element | ReactElement | FunctionComponent,
-  rightComponent?: JSX.Element | ReactElement | FunctionComponent,
+  backgroundColor?: string;
+  height?: string;
+  leftComponent?: JSX.Element | ReactElement | FunctionComponent;
+  midComponent?: JSX.Element | ReactElement | FunctionComponent;
+  rightComponent?: JSX.Element | ReactElement | FunctionComponent;
 };
-
 
 const Header: React.FC<Props> = ({
   leftComponent,
@@ -22,21 +20,26 @@ const Header: React.FC<Props> = ({
   backgroundColor,
   height,
 }) => {
+  // console.log(typeof(leftComponent));
 
-  console.log(typeof(leftComponent));
-
-  let navigation = useNavigation()
+  let navigation = useNavigation();
   return (
-    <View className={`flex  flex-row  justify-between w-full py-2 ${backgroundColor} ${height}`}>
+    <View
+      className={`flex  flex-row  justify-between w-full py-2 ${backgroundColor} ${height}`}
+    >
       <View className={leftComponent == undefined ? "w-8" : ""}>
-        {typeof(leftComponent) === 'function' ? leftComponent() : leftComponent}
+        {typeof leftComponent === "function"
+          ? leftComponent({})
+          : leftComponent}
       </View>
-      
-      <View >
-        {typeof(midComponent) === 'function' ? midComponent() : midComponent}
+
+      <View>
+        {typeof midComponent === "function" ? midComponent({}) : midComponent}
       </View>
       <View className={rightComponent == undefined ? "w-8" : ""}>
-        {typeof(rightComponent) === 'function' ? rightComponent() : rightComponent}
+        {typeof rightComponent === "function"
+          ? rightComponent({})
+          : rightComponent}
       </View>
     </View>
   );
