a0396d138ffa67126379d6fa11c49a65346156b2
nachat-ayoub <venomx2020x@gmail.com>
2022-12-01 14:49:47 +0100

    New Commit

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,7 +1,14 @@
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { ParamListBase } from "@react-navigation/native";
-import { AndroidSafeAreaView, GoBack } from "../components";
-import { View, Text } from "react-native";
+import { AndroidSafeAreaView, GoBack, Header } from "../components";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
+} from "react-native";
 import React from "react";
 
 type Props = {
@@ -11,10 +18,30 @@ type Props = {
 const Login: React.FC<Props> = ({ navigation }) => {
   return (
     <AndroidSafeAreaView removePaddingTop>
-      {/* <View className="flex-1 justify-start items-center bg-gray-200"> */}
-      {/* <GoBack back={() => navigation.goBack()} /> */}
-      <Text className="font-bold text-2xl">Login Page</Text>
-      {/* </View> */}
+      <AndroidSafeAreaView className="flex flex-1 w-full px-2">
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          className="flex-1"
+        >
+          <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View className="flex-1">
+              <Header
+                leftComponent={<GoBack back={() => navigation.goBack()} />}
+              />
+
+              <View className="flex-1 p-4">
+                <Text className="text-xl font-extrabold text-primary-600">
+                  Joby
+                </Text>
+                <Text className="text-2xl font-bold mt-3">Registration</Text>
+                <Text className="text-gray-400">
+                  Let’s Register. Apply to jobs!
+                </Text>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+      </AndroidSafeAreaView>
     </AndroidSafeAreaView>
   );
 };
